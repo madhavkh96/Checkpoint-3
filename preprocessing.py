be3d4361dd4e8c59c72246486baac00f5e09f806
Madhav <madhavkhanna2021@u.northwestern.edu>
2020-11-30 05:20:40 -0600

    Police shift data

--- preprocessing.py
+++ preprocessing.py
@@ -1,35 +1,45 @@
+# pip install json
 import json
 
-file = open("Results.csv", "r+")
+# pip install psycopg2
+import psycopg2
 
-total_entries = 274
+def rowToDict(row):
 
-lines = []
+    # update these fields as needed
+    return {
+        'officer_id': row[0],
+        'salary': row[1],
+        'pay_grade': row[2],
+        'rank_changed': row[3],
+        'rank': row[4],
+        'year': row[5]
+    }
 
-for line in file:
-    lines.append(line)
 
-x = 0
+if __name__ == '__main__':
+    with open('secrets.json') as f:
+      secrets = json.load(f)
 
-changedline = []
+    connection = psycopg2.connect(
+        database="cpdb",
+        user="cpdb-student",
+        password=secrets['db_password'],
+        host=secrets['db_host'],
+        port='5432'
+    )
 
-for line in lines:
-    line = line.replace("\"", "")
+    query = """select doaa.race, daa.area_id, EXTRACT(YEAR FROM CAST(doaa.shift_start AS DATE)), count(*) as year from data_officerassignmentattendance as doaa
+inner join data_officer d on d.id = doaa.officer_id
+inner join data_officerallegation doa on d.id = doa.officer_id
+inner join data_allegation_areas daa on doa.allegation_id = daa.allegation_id
+where doaa.race is not null and doaa.shift_start is not null
+GROUP BY doaa.race, daa.area_id, EXTRACT(YEAR FROM CAST(doaa.shift_start AS DATE))"""
 
-    if x != 0:
-        line = "," + "\"" + str(x) + "\"" + ': ' + line[31:]
-    else:
-        line = "\"" + str(x) + "\"" + ': ' + line[31:]
+    cur = connection.cursor()
+    cur.execute(query)
 
-    line = line[:-2]
-    x += 1
-    changedline.append(line)
-
-filetxt = ''.join(changedline)
-
-filetxt = '{"type":"MultiPolygon","coordinates": {' + filetxt + '}}'
-
-print(filetxt)
-
-with open('test_1.json', 'w') as json_file:
-    json.dump(filetxt, json_file)
+    with open('python_results.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        for r in results:
+            writer.writerow(list(r))
